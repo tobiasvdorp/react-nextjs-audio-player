afe86cabe6e0ed195f55e48212bdbeefdf7cbbf2
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-10-09 16:02:54 +0200

    Cleanup and Tailwind setup

--- nextjs-audio-player/tailwind.config.js
+++ nextjs-audio-player/tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
+
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      // Darkmode
+      background_dark: "#00000F",
+      primary_dark: "#207EC5",
+      secondary_dark: "#072640",
+      accent_dark: "#4D96E0",
+
+      // Lightmode
+      background: "#F6FAFE",
+      primary: "#3A97DF",
+      secondary: "#072640",
+      accent: "#1F69B2",
+
+      // Text
+      black: "#00000F",
+      white: "#F6FAFE",
     },
+    extend: {},
   },
-  plugins: [],
-}
+};
